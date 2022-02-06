3e3f193cb91786e37987b48025f482849403d172
Tobias Mahnert <tobiasmahnert@web.de>
2022-02-06 17:35:10 +1100

    word finder edits

--- word_finde.go
+++ word_finde.go
@@ -12,9 +12,14 @@ func main() {
 	words := strings.Fields(corpus)
 	query := os.Args[1:]
 
+queries:
 	for _, q := range query {
-	queries:
+	search:
 		for i, w := range words {
+			switch q {
+			case "and", "or", "the":
+				break search
+			}
 			if q == w {
 				fmt.Printf("#%-2d: %q\n", i+1, w)
 				continue queries
